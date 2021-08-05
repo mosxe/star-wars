5f3f8e194a710f9d1b4122d3a7573417e48ab521
Kirill.Alekseev <mosxe@mail.ru>
2021-08-05 18:25:10 +0300

    optimization HOC

--- src/components/hoc-helpers/with-swapi-service.js
+++ src/components/hoc-helpers/with-swapi-service.js
@@ -1,19 +1,19 @@
 ﻿import React from 'react';
 import { SwapiServiceConsumer } from '../swapi-service-context';
 
-const withSwapiService = (Wrapped) => {
+const withSwapiService = (Wrapped, mapMethodsToProps) => {
   return (props) => {
     return (<SwapiServiceConsumer>
       {
         (swapiService) => {
+          const serviceProps = mapMethodsToProps(swapiService);
           return (
-            <Wrapped {...props} swapiService={swapiService}/> 
+            <Wrapped {...props} {...serviceProps}/> 
           )
         }
       }
     </SwapiServiceConsumer>);
   }
 };
-
 
 export default withSwapiService;
--- src/components/hoc-helpers/withData.js
+++ src/components/hoc-helpers/withData.js
@@ -1,20 +1,20 @@
 ﻿import React, { Component } from 'react';
 import Spinner from '../spinner';
-import ErrorIndicator from '../error-indicator';
 
-const withData = (View, getData) => {
+const withData = (View) => {
   return class extends Component {
     state = {
       data: null
     };
   
     componentDidMount() {
-      getData().then(data => {
+      this.props.getData().then(data => {
         this.setState({
           data
         });
       });
-    }
+    };
+    
     render() {
       const  { data } = this.state;
 
